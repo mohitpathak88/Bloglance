97fae6ba865d9e976bf3d4021deceddbb7b554f4
harshit <h8pathak@gmail.com>
2020-07-09 22:45:35 +0530

    Inherited base file and added meta tags and scripts

--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Mohit Pathak',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 29, 2020' 
+    },
+    {
+        'author': 'Harshit Pathak',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 30, 2020' 
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'myblog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'myblog/about.html', {'title': 'About'})
 
